4f44312f91b8342e9a6e3854a34ce26ef4359a2c
zivp <zivp1010@gmail.com>
2020-02-17 23:05:11 +0200

    ins

---
+++ Framework/src/main/java/pages/BasePage.java
@@ -0,0 +1,21 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasePage {
+	
+static WebDriver driver;
+static String driverPath = "C:\\Users\\win-8\\Desktop\\SeleniumDrivers\\chromedriver.exe";
+
+private static WebDriver initChromeDriver(String appURL) {
+	System.out.println("Launching google chrome with new profile..");
+	System.setProperty("webdriver.chrome.driver", driverPath
+			+ "chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.navigate().to(appURL);
+	return driver;
+}
+
+}
